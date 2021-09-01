5cb44f7f55272fdae913593da0d208f4c454f2a3
myPrimryNerve <88952460+myPrimryNerve@users.noreply.github.com>
2021-09-01 20:58:25 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,81 @@
+import math
+import pygame
+
+pygame.init()
+
+displayWidth = 1280
+displayHeight = 720
+
+display = pygame.display.set_mode((displayWidth, displayHeight))
+
+posSwordX = 640
+posSwordY = 560
+startAngle = 0
+
+pygame.display.set_caption('Sword bouncer')
+
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+mcWidth = 40
+mcHeight = 60
+mcX = displayWidth / 2
+mcY = displayHeight / 1.35
+
+enemyWidth = 128
+enemyHeight = 184
+enemyX = displayWidth - 50
+enemyY = 408
+
+fpsLimiter = pygame.time.Clock()
+
+def angles():
+    global posSwordX, posSwordY, startAngle
+    angle = startAngle * 3.14 / 180
+    posSwordX = (150 * math.cos(angle)) + 620  # 640 < начальная точка по x
+    posSwordY = (250 * math.sin(angle)) + 500  # 560 < начальная точка по у
+
+def runGame():
+    global posSwordX, posSwordY, startAngle
+
+    bg = pygame.image.load('bg.png')
+    land = pygame.image.load('land.png')
+    sword = pygame.image.load('sword.png')
+    mc = pygame.image.load('mc.png')
+
+    game = True
+
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            angles()
+            startAngle -= 4
+        elif keys[pygame.K_d]:
+            angles()
+            startAngle += 4
+
+        display.blit(bg, (0, 0))
+        drawEnemy()
+        display.blit(mc, (576, 400))
+        display.blit(sword, (int(posSwordX), int(posSwordY)))
+        display.blit(land, (0, 592))
+
+
+        pygame.display.update()
+        fpsLimiter.tick(60)
+
+def drawEnemy():
+    global enemyX, enemyY, enemyWidth, enemyHeight
+
+    if enemyX >= -enemyWidth:
+        pygame.draw.rect(display, (0, 255, 255), (enemyX, enemyY, enemyWidth, enemyHeight))
+        enemyX -= 4
+    else:
+        enemyX = displayWidth - 50
+
+runGame()
