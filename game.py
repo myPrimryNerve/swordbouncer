905922915c84e0a79728b174ab5d81aecf671b1f
myPrimryNerve <88952460+myPrimryNerve@users.noreply.github.com>
2021-09-02 20:21:58 +0300

    update

--- game.py
+++ game.py
@@ -8,11 +8,12 @@ displayHeight = 720
 
 display = pygame.display.set_mode((displayWidth, displayHeight))
 
-posSwordX = 640
-posSwordY = 560
+posSwordX = 620 #636
+posSwordY = 440 #504
 startAngle = 0
 
 pygame.display.set_caption('Sword bouncer')
+sword = pygame.image.load('sword.png')
 
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
@@ -29,23 +30,27 @@ enemyY = 408
 
 fpsLimiter = pygame.time.Clock()
 
-def rotCenter(image, rect, angle):
-        rotImage = pygame.transform.rotate(image, angle)
-        rotRect = rotImage.get_rect(center=rect.center)
-        return rotImage,rotRect
+def rotate(angle):
+    global sword
+    sword = pygame.transform.rotate(sword, angle)
+    new_rect = sword.get_rect(center=sword.get_rect(center=(int(posSwordX)+16, int(posSwordY)+64)).center)
+    rotated_image = pygame.transform.rotate(sword, angle)
+    new_rect = rotated_image.get_rect(center=sword.get_rect(topleft=(int(posSwordX)-32, int(posSwordY)-128)).center)
+    display.blit(sword, new_rect.topleft)
+
+    return sword, new_rect
 
 def angles():
     global posSwordX, posSwordY, startAngle
     angle = startAngle * 3.14 / 180
-    posSwordX = (150 * math.cos(angle)) + 620  # 640 < начальная точка по x
-    posSwordY = (250 * math.sin(angle)) + 500  # 560 < начальная точка по у
+    posSwordX = (150 * math.cos(angle)) + 620  # 620 < начальная точка по x
+    posSwordY = (250 * math.sin(angle)) + 440  # 540 < начальная точка по у
 
 def runGame():
     global posSwordX, posSwordY, startAngle
 
     bg = pygame.image.load('bg.png')
     land = pygame.image.load('land.png')
-    sword = pygame.image.load('sword.png')
     mc = pygame.image.load('mc.png')
 
     game = True
@@ -58,16 +63,12 @@ def runGame():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
+            rotate(-4)
             angles()
-            rect = sword.get_rect(center=(int(posSwordX), int(posSwordY)))
-            surf, r = rotCenter(sword, rect, -4)
-            display.blit(surf, r)
             startAngle -= 4
         elif keys[pygame.K_d]:
+            rotate(4)
             angles()
-            rect = sword.get_rect(center=(int(posSwordX), int(posSwordY)))
-            surf, r = rotCenter(sword, rect, 4)
-            display.blit(surf, r)
             startAngle += 4
 
         display.blit(bg, (0, 0))
